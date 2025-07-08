0fa3eda1291fe74885668c7b60f477ca9d15cc49
kulkarnisanika <kulkarnisanika2@gmail.com>
2025-07-08 21:41:30 +0530

    drag and drop implemented

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -3,7 +3,17 @@ import StatusCard from './StatusCard'
 import { Box, Grid, Stack } from '@mui/material'
 
 const CardContainer = ({ cardsData = [] }) => {
+
+     /*
+    onDrop => columnName, index
+    to get that item - todoList[TargetColumnName][TargetIndex]
+
+    source Item = clonedList[sourceColumn].splice(sourceIndex,1)
+    Add that = clonedList[targetColum].splice(targetIndex,0,sourceItem)
+
+    */
     const [formattedData, setFormattedData] = useState({})
+    const [draggedItem, setDraggedItem] = useState(null);
     useEffect(() => {
         const organizedData = cardsData?.reduce((acc, item) => {
             const { status } = item;
@@ -20,25 +30,46 @@ const CardContainer = ({ cardsData = [] }) => {
         }, {});
         setFormattedData(organizedData);
     }, [cardsData]);
-    const statusMap = {
-        new: 'Not Started',
-        inProgress: 'In Progress',
-        completed: 'Completed'
-    }
+
     const orderedStatuses = ['new', 'inProgress', 'completed'];
+
+    const handleOnDropItem = (targetColumn, targetIndex)  => {
+        const {sourceColumn, sourceItem, sourceIndex} = draggedItem
+        const targetItem = formattedData[targetColumn][targetIndex]
+        if(!sourceItem || (sourceItem === targetItem)) return;
+
+        //code to drop
+        //1. clone into another var
+        const clonnedItemList = formattedData;
+        //2. remove source item from source column
+       clonnedItemList[sourceColumn]?.splice(sourceIndex,1);
+        //3. Add draggedItem into target column at target index
+       clonnedItemList[targetColumn]?.splice(targetIndex,0,sourceItem);
+
+        setDraggedItem(clonnedItemList)
+    }
     return (
         <Box sx={{ width: '100vw' }}>
             <Grid container gap={3}>
-                {orderedStatuses.map((statusKey) => {
+                {orderedStatuses.map((statusKey,index) => {
                     const cards = formattedData[statusKey];
                     if (!cards) return null;
                     return (
                         <Grid item key={statusKey} sx={{ flex: 1 }}>
-                            <StatusCard
-                                cardData={cards}
-                                status={statusMap[statusKey]}
-                                sx={{ flex: 1 }}
-                            />
+                            <div >
+                                <StatusCard
+                                    cardData={cards}
+                                    status={statusKey}
+                                    sx={{ flex: 1 }}
+                                    handleOnDragStart={(sourceIndex) => {
+                                        //code to set draggableItem
+                                    }}
+                                    handleOnDrop = {
+                                        () => handleOnDropItem(statusKey,index)
+                                    }
+                                    setDraggedItem={setDraggedItem}
+                                />
+                            </div>
                         </Grid>
                     );
                 })}
--- src/components/StatusCard.jsx
+++ src/components/StatusCard.jsx
@@ -2,21 +2,44 @@ import { Box, Card, Stack, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import TaskCard from './TaskCard';
 
-const StatusCard = ({ status, cardData }) => {
+const StatusCard = ({ status, cardData, handleOnDragStart,handleOnDrop, setDraggedItem }) => {
 
+   
+    const statusMap = {
+        new: 'Not Started',
+        inProgress: 'In Progress',
+        completed: 'Completed'
+    }
+   
+    
     return (
-        <Stack gap={2}>
-            <Typography sx={{ fontSize: '2', color:'#3B373E'}}>{status}</Typography>
-            <Box sx={{ minHeight: '606px', backgroundColor: '#C5C0C94D', borderRadius: 2, p:2 }}>
-                <Stack gap={2} >
-                    {
-                        cardData?.map((item) => {
-                            return (<TaskCard key={item?.id}  title={item?.title} description={item?.description} tag></TaskCard>)
-                        })
-                    }
-                </Stack>
-            </Box>
-        </Stack>
+        <div onDrop={(handleOnDrop)}
+            onDragOver={(e) => e.preventDefault()}>
+            <Stack gap={2}>
+                <Typography sx={{ fontSize: '2', color: '#3B373E' }}>{statusMap[status]}</Typography>
+                <Box sx={{ minHeight: '606px', backgroundColor: '#C5C0C94D', borderRadius: 2, p: 2 }}>
+                    <Stack gap={2} >
+                        {
+                            cardData?.map((item,index) => {
+                                return (
+                                    <div key={item?.id}
+                                        draggable
+                                        onDragStart={() => setDraggedItem({
+                                            sourceColumn: status,
+                                            sourceItem: item,
+                                            sourceIndex: index
+                                        })}
+                                        onDragOver={(e) => e.preventDefault()}
+                                        style={{ cursor: "move" }}>
+                                        <TaskCard title={item?.title} description={item?.description} tag></TaskCard>
+                                    </div>
+                                )
+                            })
+                        }
+                    </Stack>
+                </Box>
+            </Stack>
+        </div>
     )
 }
 
